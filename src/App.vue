880a8435612a5b3adde743389e30cbcbd950432b
Arnaud Fougerousse <arnaudfgr@gmail.com>
2020-06-14 19:26:16 +0200

    start carousel

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+  <carousel>
+    <carousel-slide>
+      Salut les gens
+    </carousel-slide>
+    <carousel-slide>
+      Aurevoir les gens
+    </carousel-slide>
+  </carousel>
+  </div>
+</template>
+
+<script>
+import Carousel from './components/carousel/Carousel'
+import CarouselSlide from './components/carousel/CarouselSlide'
+
+export default {
+  name: 'App',
+  components: {
+    Carousel,
+    CarouselSlide
+  }
+}
+</script>
---
+++ src/components/carousel/Carousel.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="carousel">
+    <slot></slot>
+    <button class="carousel__nav carousel__prev" @click.prevent="prev">Précédent</button>
+    <button class="carousel__nav carousel__next" @click.prevent="next">Suivant</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      index: 0,
+      slides: []
+    }
+  },
+
+  mounted (){
+    this.slides = this.$children
+    this.slides.forEach((slide, i) => {
+      slide.index = i
+    })
+  },
+
+  computed: {
+    slidesCount () { return this.slides.length }
+  },
+
+  methods: {
+    next(){
+      this.index++
+      if(this.index >= this.slidesCount){
+        this.index = 0
+      }
+    },
+    prev(){
+      this.index--
+      if(this.index < 0){
+        this.index = this.slidesCount - 1
+      }
+    },
+
+  }
+}
+</script>
